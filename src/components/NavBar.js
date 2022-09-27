e440a6462a442bd3e4852bdc39c612f3813de4f3
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-09-27 06:05:45 +0300

    completed react hooks list and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkItems = links.map((link) => {
+    return <li key={link} style={{listStyleType: "none"}}>#{link}</li>
+  })
+  console.log(linkItems)
+
+  return <nav>
+    {links.map((link) => {
+    return (
+      <a key={link} href={"#" + link}>{link}</a>
+    )
+    })}
+    </nav>;
 }
 
 export default NavBar;
