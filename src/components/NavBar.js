c192da73af72c03529c161a4993a03d5286d7687
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-09-27 07:14:03 +0300

    made some changes to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,6 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  const linkItems = links.map((link) => {
-    return <li key={link} style={{listStyleType: "none"}}>#{link}</li>
-  })
-  console.log(linkItems)
-
   return <nav>
     {links.map((link) => {
     return (
